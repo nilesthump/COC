6f381e3f58ed5053f6ddd43df27a95d49c6cb5bc
nilesthump <2963862605@qq.com>
2025-12-13 22:02:32 +0800

    SecondScene.h 添加移动背景

--- Classes/SecondScene.h
+++ Classes/SecondScene.h
@@ -8,10 +8,16 @@ class SecondScene :public cocos2d::Scene
 public:
 	//声明
 	static cocos2d::Scene* createScene();
-
+	
 	bool virtual init();
 	void menuFirstCallback(cocos2d::Ref* pSender);
 	void menuBuildCallback(Ref* pSender);
+
+	//触摸事件处理函数
+	bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* event);
 
 	CREATE_FUNC(SecondScene);
 private:
