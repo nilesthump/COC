a8caebcd6fbbef64f123599f96c9b86862fd61cd
nilesthump <2963862605@qq.com>
2025-12-22 00:19:47 +0800

    修改了登录界面ui

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -4,6 +4,8 @@
 
 USING_NS_CC;
 
+// Initialize static login status variable
+bool HelloWorld::isLoggedIn = false;
 
 Scene* HelloWorld::createScene()
 {
@@ -50,7 +52,7 @@ bool HelloWorld::init()
     }
 
 
-    auto secondSceneItem = MenuItemImage::create(
+    secondSceneItem = MenuItemImage::create(
         "btn_normal.png",      
         "btn_pressed.png",     
         CC_CALLBACK_1(HelloWorld::menuSecondSceneCallback, this));
@@ -76,7 +78,7 @@ bool HelloWorld::init()
         secondSceneItem->addChild(secondLabel);
     }
 
-    auto battleTestItem = MenuItemImage::create(
+    battleTestItem = MenuItemImage::create(
         "btn_normal.png",
         "btn_pressed.png",
         CC_CALLBACK_1(HelloWorld::menuBattleTestCallback, this));
@@ -102,10 +104,84 @@ bool HelloWorld::init()
         battleTestItem->addChild(testLabel);
     }
 
-    auto menu = Menu::create(closeItem, secondSceneItem,battleTestItem, NULL);  
+
+    // Create login button
+    loginItem = MenuItemImage::create(
+        "btn_normal.png",
+        "btn_pressed.png",
+        CC_CALLBACK_1(HelloWorld::menuLoginCallback, this));
+
+    if (loginItem == nullptr ||
+        loginItem->getContentSize().width <= 0 ||
+        loginItem->getContentSize().height <= 0)
+    {
+        problemLoading("'btn_normal.png' and 'btn_pressed.png'");
+    }
+    else
+    {
+        double x = origin.x + visibleSize.width / 2;
+        double y = origin.y + visibleSize.height / 4;
+        loginItem->setPosition(Vec2(x, y));
+
+        loginLabel = Label::createWithSystemFont("LOGIN", "fonts/Marker Felt.ttf", 24);
+        loginLabel->setColor(Color3B::WHITE);
+        loginLabel->setPosition(Vec2(loginItem->getContentSize().width / 2,
+            loginItem->getContentSize().height / 2));
+        loginItem->addChild(loginLabel);
+    }
+
+    auto menu = Menu::create(closeItem, secondSceneItem, battleTestItem, loginItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
+    // Initialize login UI components
+    loginLayer = nullptr;
+    logoutConfirmLayer = nullptr;
+    usernameEditBox = nullptr;
+    passwordEditBox = nullptr;
+    confirmItem = nullptr;
+    confirmLogoutItem = nullptr;
+    cancelLogoutItem = nullptr;
+
+    // Initially hide and disable secondSceneItem and battleTestItem
+    if (isLoggedIn)
+    {
+        // Logged in state
+        if (secondSceneItem != nullptr)
+        {
+            secondSceneItem->setVisible(true);
+            secondSceneItem->setEnabled(true);
+        }
+
+        if (battleTestItem != nullptr)
+        {
+            battleTestItem->setVisible(true);
+            battleTestItem->setEnabled(true);
+        }
+
+        // Change login button to logout button
+        loginItem->setCallback(CC_CALLBACK_1(HelloWorld::menuLogoutCallback, this));
+        loginLabel->setString("LOGOUT");
+    }
+    else
+    {
+        // Not logged in state
+        if (secondSceneItem != nullptr)
+        {
+            secondSceneItem->setVisible(false);
+            secondSceneItem->setEnabled(false);
+        }
+
+        if (battleTestItem != nullptr)
+        {
+            battleTestItem->setVisible(false);
+            battleTestItem->setEnabled(false);
+        }
+
+        // Ensure login button is in login state
+        loginItem->setCallback(CC_CALLBACK_1(HelloWorld::menuLoginCallback, this));
+        loginLabel->setString("LOGIN");
+    }
     /////////////////////////////
     // 3. add your codes below...
 
@@ -215,4 +291,244 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
     //_eventDispatcher->dispatchEvent(&customEndEvent);
 
 
+}
+
+void HelloWorld::menuLoginCallback(cocos2d::Ref* pSender)
+{
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    // Create a semi-transparent background layer
+    if (loginLayer == nullptr)
+    {
+        loginLayer = LayerColor::create(Color4B(0, 0, 0, 180));
+        this->addChild(loginLayer, 10);
+
+        // Create username label
+        usernameLabel = Label::createWithSystemFont("Account:", "fonts/Marker Felt.ttf", 20);
+        usernameLabel->setColor(Color3B::WHITE);
+        usernameLabel->setPosition(Vec2(origin.x + visibleSize.width / 2 - 150,
+            origin.y + visibleSize.height / 2 + 50));
+        loginLayer->addChild(usernameLabel);
+
+        // Create username edit box
+        usernameEditBox = ui::EditBox::create(Size(300, 40), "btn_normal.png");
+        usernameEditBox->setPosition(Vec2(origin.x + visibleSize.width / 2,
+            origin.y + visibleSize.height / 2 + 50));
+        usernameEditBox->setPlaceholderFontName("fonts/Marker Felt.ttf");
+        usernameEditBox->setPlaceholderFontSize(20);
+        usernameEditBox->setPlaceHolder("Enter your account name");
+        usernameEditBox->setFontName("fonts/Marker Felt.ttf");
+        usernameEditBox->setFontSize(20);
+        usernameEditBox->setFontColor(Color3B::WHITE);
+        usernameEditBox->setReturnType(ui::EditBox::KeyboardReturnType::NEXT);
+        usernameEditBox->setDelegate(this);
+        loginLayer->addChild(usernameEditBox);
+
+        // Create password label
+        passwordLabel = Label::createWithSystemFont("Password:", "fonts/Marker Felt.ttf", 20);
+        passwordLabel->setColor(Color3B::WHITE);
+        passwordLabel->setPosition(Vec2(origin.x + visibleSize.width / 2 - 150,
+            origin.y + visibleSize.height / 2 - 20));
+        loginLayer->addChild(passwordLabel);
+
+        // Create password edit box
+        passwordEditBox = ui::EditBox::create(Size(300, 40), "btn_normal.png");
+        passwordEditBox->setPosition(Vec2(origin.x + visibleSize.width / 2,
+            origin.y + visibleSize.height / 2 - 20));
+        passwordEditBox->setPlaceholderFontName("fonts/Marker Felt.ttf");
+        passwordEditBox->setPlaceholderFontSize(20);
+        passwordEditBox->setPlaceHolder("Enter your password");
+        passwordEditBox->setFontName("fonts/Marker Felt.ttf");
+        passwordEditBox->setFontSize(20);
+        passwordEditBox->setFontColor(Color3B::WHITE);
+        passwordEditBox->setInputFlag(ui::EditBox::InputFlag::PASSWORD);
+        passwordEditBox->setInputMode(ui::EditBox::InputMode::SINGLE_LINE);
+        passwordEditBox->setReturnType(ui::EditBox::KeyboardReturnType::DONE);
+        passwordEditBox->setDelegate(this);
+        loginLayer->addChild(passwordEditBox);
+
+        // Create confirm button
+        confirmItem = MenuItemImage::create(
+            "btn_normal.png",
+            "btn_pressed.png",
+            CC_CALLBACK_1(HelloWorld::menuConfirmCallback, this));
+
+        if (confirmItem != nullptr &&
+            confirmItem->getContentSize().width > 0 &&
+            confirmItem->getContentSize().height > 0)
+        {
+            double x = origin.x + visibleSize.width / 2;
+            double y = origin.y + visibleSize.height / 2 - 90;
+            confirmItem->setPosition(Vec2(x, y));
+
+            auto confirmLabel = Label::createWithSystemFont("Confirm", "fonts/Marker Felt.ttf", 24);
+            confirmLabel->setColor(Color3B::WHITE);
+            confirmLabel->setPosition(Vec2(confirmItem->getContentSize().width / 2,
+                confirmItem->getContentSize().height / 2));
+            confirmItem->addChild(confirmLabel);
+
+            auto confirmMenu = Menu::create(confirmItem, NULL);
+            confirmMenu->setPosition(Vec2::ZERO);
+            loginLayer->addChild(confirmMenu);
+        }
+    }
+    else
+    {
+        // Show the existing login layer if it was already created
+        loginLayer->setVisible(true);
+    }
+}
+
+void HelloWorld::menuConfirmCallback(cocos2d::Ref* pSender)
+{
+    // Hide login layer
+    if (loginLayer != nullptr)
+    {
+        loginLayer->setVisible(false);
+    }
+
+    // Show secondSceneItem and battleTestItem
+    if (secondSceneItem != nullptr)
+    {
+        secondSceneItem->setVisible(true);
+        secondSceneItem->setEnabled(true);
+    }
+
+    if (battleTestItem != nullptr)
+    {
+        battleTestItem->setVisible(true);
+        battleTestItem->setEnabled(true);
+    }
+
+    // Update login status
+    isLoggedIn = true;
+
+    // Change login button to logout button
+    loginItem->setCallback(CC_CALLBACK_1(HelloWorld::menuLogoutCallback, this));
+    loginLabel->setString("LOGOUT");
+}
+
+// EditBox delegate functions implementation
+void HelloWorld::editBoxEditingDidBegin(cocos2d::ui::EditBox* editBox)
+{
+    // Handle edit box begin editing
+}
+
+void HelloWorld::editBoxTextChanged(cocos2d::ui::EditBox* editBox, const std::string& text)
+{
+    // Handle edit box text change
+}
+
+void HelloWorld::editBoxReturn(cocos2d::ui::EditBox* editBox)
+{
+    // Handle edit box return key pressed
+}
+
+void HelloWorld::editBoxEditingDidEndWithAction(cocos2d::ui::EditBox* editBox, cocos2d::ui::EditBoxDelegate::EditBoxEndAction action)
+{
+    // Handle edit box end editing with action
+}
+
+void HelloWorld::menuLogoutCallback(cocos2d::Ref* pSender)
+{
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    // Create a semi-transparent background layer for confirmation dialog
+    if (logoutConfirmLayer == nullptr)
+    {
+        logoutConfirmLayer = LayerColor::create(Color4B(0, 0, 0, 180));
+        this->addChild(logoutConfirmLayer, 10);
+
+        // Create confirm logout button
+        confirmLogoutItem = MenuItemImage::create(
+            "btn_normal.png",
+            "btn_pressed.png",
+            CC_CALLBACK_1(HelloWorld::menuConfirmLogoutCallback, this));
+
+        if (confirmLogoutItem != nullptr &&
+            confirmLogoutItem->getContentSize().width > 0 &&
+            confirmLogoutItem->getContentSize().height > 0)
+        {
+            double x = origin.x + visibleSize.width / 2 - 120;
+            double y = origin.y + visibleSize.height / 2;
+            confirmLogoutItem->setPosition(Vec2(x, y));
+
+            auto confirmLabel = Label::createWithSystemFont("Confirm", "fonts/Marker Felt.ttf", 24);
+            confirmLabel->setColor(Color3B::WHITE);
+            confirmLabel->setPosition(Vec2(confirmLogoutItem->getContentSize().width / 2,
+                confirmLogoutItem->getContentSize().height / 2));
+            confirmLogoutItem->addChild(confirmLabel);
+        }
+
+        // Create cancel logout button
+        cancelLogoutItem = MenuItemImage::create(
+            "btn_normal.png",
+            "btn_pressed.png",
+            CC_CALLBACK_1(HelloWorld::menuCancelLogoutCallback, this));
+
+        if (cancelLogoutItem != nullptr &&
+            cancelLogoutItem->getContentSize().width > 0 &&
+            cancelLogoutItem->getContentSize().height > 0)
+        {
+            double x = origin.x + visibleSize.width / 2 + 120;
+            double y = origin.y + visibleSize.height / 2;
+            cancelLogoutItem->setPosition(Vec2(x, y));
+
+            auto cancelLabel = Label::createWithSystemFont("Cancel", "fonts/Marker Felt.ttf", 24);
+            cancelLabel->setColor(Color3B::WHITE);
+            cancelLabel->setPosition(Vec2(cancelLogoutItem->getContentSize().width / 2,
+                cancelLogoutItem->getContentSize().height / 2));
+            cancelLogoutItem->addChild(cancelLabel);
+        }
+
+        // Create menu for the logout confirmation buttons
+        auto logoutConfirmMenu = Menu::create(confirmLogoutItem, cancelLogoutItem, NULL);
+        logoutConfirmMenu->setPosition(Vec2::ZERO);
+        logoutConfirmLayer->addChild(logoutConfirmMenu);
+    }
+    else
+    {
+        // Show the existing logout confirmation layer if it was already created
+        logoutConfirmLayer->setVisible(true);
+    }
+}
+
+void HelloWorld::menuConfirmLogoutCallback(cocos2d::Ref* pSender)
+{
+    // Hide logout confirmation layer
+    if (logoutConfirmLayer != nullptr)
+    {
+        logoutConfirmLayer->setVisible(false);
+    }
+
+    // Change logout button back to login button
+    loginItem->setCallback(CC_CALLBACK_1(HelloWorld::menuLoginCallback, this));
+    loginLabel->setString("LOGIN");
+
+    // Hide and disable secondSceneItem and battleTestItem
+    if (secondSceneItem != nullptr)
+    {
+        secondSceneItem->setVisible(false);
+        secondSceneItem->setEnabled(false);
+    }
+
+    if (battleTestItem != nullptr)
+    {
+        battleTestItem->setVisible(false);
+        battleTestItem->setEnabled(false);
+    }
+
+    // Update login status
+    isLoggedIn = false;
+}
+
+void HelloWorld::menuCancelLogoutCallback(cocos2d::Ref* pSender)
+{
+    // Hide logout confirmation layer and keep login status
+    if (logoutConfirmLayer != nullptr)
+    {
+        logoutConfirmLayer->setVisible(false);
+    }
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,8 +2,10 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "ui/UIWidget.h"
+#include "ui/UIEditBox/UIEditBox.h"
 
-class HelloWorld : public cocos2d::Scene
+class HelloWorld : public cocos2d::Scene,public cocos2d::ui::EditBoxDelegate
 {
 public:
     static cocos2d::Scene* createScene();
@@ -14,8 +16,41 @@ public:
     void menuCloseCallback(cocos2d::Ref* pSender);
     void menuSecondSceneCallback(cocos2d::Ref* pSender);
     void menuBattleTestCallback(cocos2d::Ref* pSender);
+    void menuLoginCallback(cocos2d::Ref* pSender);
+    void menuConfirmCallback(cocos2d::Ref* pSender);
+    void menuLogoutCallback(cocos2d::Ref* pSender);
+    void menuConfirmLogoutCallback(cocos2d::Ref* pSender);
+    void menuCancelLogoutCallback(cocos2d::Ref* pSender);
+
+    // EditBox delegate functions
+    virtual void editBoxEditingDidBegin(cocos2d::ui::EditBox* editBox) override;
+    virtual void editBoxTextChanged(cocos2d::ui::EditBox* editBox, const std::string& text) override;
+    virtual void editBoxReturn(cocos2d::ui::EditBox* editBox) override;
+    virtual void editBoxEditingDidEndWithAction(cocos2d::ui::EditBox* editBox,
+        cocos2d::ui::EditBoxDelegate::EditBoxEndAction action) override;
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+private:
+    cocos2d::MenuItemImage* secondSceneItem;
+    cocos2d::MenuItemImage* battleTestItem;
+    cocos2d::MenuItemImage* loginItem;
+    cocos2d::MenuItemImage* confirmItem;
+
+    cocos2d::MenuItemImage* confirmLogoutItem;
+    cocos2d::MenuItemImage* cancelLogoutItem;
+
+    cocos2d::ui::EditBox* usernameEditBox;
+    cocos2d::ui::EditBox* passwordEditBox;
+
+    cocos2d::LayerColor* loginLayer;
+    cocos2d::LayerColor* logoutConfirmLayer;
+
+    cocos2d::Label* usernameLabel;
+    cocos2d::Label* passwordLabel;
+    cocos2d::Label* loginLabel;
+
+    static bool isLoggedIn;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
