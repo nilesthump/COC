69e505d49af78450c5bab37fc81fa25d891abf1f
muMukefu <3861047092@qq.com>
2025-12-21 23:30:28 +0800

    金币和圣水

---
+++ Classes/ElixirCollector.h
@@ -0,0 +1,41 @@
+#ifndef __ELIXIR_COLLECTOR_H__
+#define __ELIXIR_COLLECTOR_H_
+
+#include "cocos2d.h"
+
+class ElixirCollector : public cocos2d::Node
+{
+private:
+    // 核心属性
+    int _hp;                  // 血量
+    float _elixirGenerateSpeed; // 产金速度
+    std::string _textureName; // 纹理名称（用于区分不同等级圣水收集器）
+    cocos2d::Sprite* _sprite; // 图像精灵
+
+    // 初始化精灵（内部调用）
+    bool initSprite(const std::string& textureName);
+
+public:
+    // 静态创建函数（Cocos推荐方式）
+    static ElixirCollector* create(const std::string& textureName, int hp = 100, float elixirSpeed = 1.0f);
+    // 初始化函数
+    virtual bool init(const std::string& textureName, int hp, float goldSpeed);
+
+    // --- 对外接口 ---
+    // 设置/获取位置（复用Node的position，锚点内部管理）
+    void setMinePosition(const cocos2d::Vec2& pos) { this->setPosition(pos); }
+    cocos2d::Vec2 getMinePosition() const { return this->getPosition(); }
+
+    // 获取精灵（仅用于特殊操作，尽量封装在类内）
+    cocos2d::Sprite* getSprite() const { return _sprite; }
+
+    // 视觉反馈：闪烁（成功放置）
+    void playSuccessBlink() { this->runAction(cocos2d::Blink::create(1.0f, 3)); }
+    // 视觉反馈：红色闪烁+销毁（失败放置）
+    void playFailBlinkAndRemove();
+
+    // 测试：获取产圣水速度（验证对象属性）
+    float getElixirSpeed() const { return _elixirGenerateSpeed;}
+};
+
+#endif
